380193475d4eb5933d4920231e44c3f233c14a8c
jesrs <jesrs@yahoo.com>
2021-04-09 17:48:07 -0500

    Primer versión
    Primer versión en donde únicamente se trabaja con el proyecto inicial y no se tiene mas que una entidad.

---
+++ src/test/java/com/readinglist/demo/ReadingListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.readinglist.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
